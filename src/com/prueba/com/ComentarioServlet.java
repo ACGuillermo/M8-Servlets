29745448eeee65e7488206aac2c87da3a986a2cc
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-04-29 14:40:34 +0200

    Clossing connections in ComentarioServlet
    Clossing connections in finally block ComentarioServlet.

--- src/com/prueba/com/ComentarioServlet.java
+++ src/com/prueba/com/ComentarioServlet.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
@@ -36,25 +37,32 @@ public class ComentarioServlet extends HttpServlet {
 		String nick = request.getParameter("nick");
 		String comentario = request.getParameter("comentario");
 		
+		Connection con = null; 
+		Statement stmt = null; 
+		ResultSet rs = null;
+		ResultSet rs2 = null;
+		PreparedStatement selectUser = null;
+		PreparedStatement insertCompra = null;
+		
 		try {
 			new org.sqlite.JDBC();
 	        Class.forName("org.sqlite.JDBC");
 	        String url = "jdbc:sqlite:C:\\Users\\Guillermo\\Desktop\\WorkStation\\M8-Servlets\\WebContent\\WEB-INF\\lib\\test.db";
-	        Connection con = DriverManager.getConnection(url);
+	        con = DriverManager.getConnection(url);
 	        System.out.println("Opened database successfully");
 	        
 	      //COMPROBAR USER EN BBDD
-	        PreparedStatement selectUser = (PreparedStatement) con.prepareStatement("select * from users where nick = ?");
+	        selectUser = (PreparedStatement) con.prepareStatement("select * from users where nick = ?");
 	        
 	        selectUser.setString(1, nick);
 	        selectUser.execute();
-	        ResultSet rs = selectUser.getResultSet();
+	        rs = selectUser.getResultSet();
 	        
 	        Boolean usuarioExiste = rs.next();
 	        
 	        if(usuarioExiste) {
 	        	
-	        	PreparedStatement insertCompra = (PreparedStatement) con.prepareStatement("INSERT INTO comentarios (nick, comentario) values (?,?)");
+	        	insertCompra = (PreparedStatement) con.prepareStatement("INSERT INTO comentarios (nick, comentario) values (?,?)");
 	        	
 	        	insertCompra.setString(1, nick);
 	        	insertCompra.setString(2, comentario);
@@ -62,8 +70,8 @@ public class ComentarioServlet extends HttpServlet {
 	        	insertCompra.executeUpdate();
 	        	
 	        	String sql = "SELECT * FROM comentarios";
-		        Statement stmt  = con.createStatement();
-	            ResultSet rs2    = stmt.executeQuery(sql);
+		        stmt  = con.createStatement();
+	            rs2    = stmt.executeQuery(sql);
 	            
 	            // loop through the result set
 	            while (rs2.next()) {
@@ -87,6 +95,43 @@ public class ComentarioServlet extends HttpServlet {
 	        
 		}catch(Exception e){
 			e.printStackTrace();
+		}finally { 
+			try { 
+		        if (rs != null) 
+		            rs.close(); 
+		    } catch (SQLException sqle) {
+		    	System.out.println(sqle);
+		    }
+			try { 
+		        if (rs2 != null) 
+		            rs2.close(); 
+		    } catch (SQLException sqle) {
+		    	System.out.println(sqle);
+		    }
+		    try { 
+		        if (stmt != null) 
+		            stmt.close(); 
+		    } catch (SQLException sqle) {
+		    	System.out.println(sqle);
+		    }
+		    try { 
+		        if (selectUser != null) 
+		        	selectUser.close(); 
+		    } catch (SQLException sqle) {
+		    	System.out.println(sqle);
+		    }
+		    try { 
+		        if (insertCompra != null) 
+		        	insertCompra.close(); 
+		    } catch (SQLException sqle) {
+		    	System.out.println(sqle);
+		    }
+		    try { 
+		        if (con != null) 
+		            con.close(); 
+		    } catch (SQLException sqle)  {
+		    	System.out.println(sqle);
+		    }
 		}
 	}
 
