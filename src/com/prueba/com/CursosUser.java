5e6ae4a95d9803d68d7f5922b20a798639792a69
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-04-30 12:43:32 +0200

    CursosUser.java: Removed unnecesary TODOs

--- src/com/prueba/com/CursosUser.java
+++ src/com/prueba/com/CursosUser.java
@@ -29,7 +29,6 @@ public class CursosUser extends HttpServlet {
      */
     public CursosUser() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -90,7 +89,6 @@ public class CursosUser extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
