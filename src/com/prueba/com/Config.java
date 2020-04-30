f8a892e73006c9f4191c4fa6495c78dd79e047a9
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-04-30 18:10:55 +0200

    Config.java: added documentation

--- src/com/prueba/com/Config.java
+++ src/com/prueba/com/Config.java
@@ -5,11 +5,25 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+/**
+ * Config.java
+ * Class implementation for retrieving the configuration from the properties file.
+ * @author  Guillermo
+ * @since   2020-04-30
+ */
 public class Config {
 	private static final Logger LOGGER = Logger.getAnonymousLogger();
-	
 	private String db = null;
 	
+	/**
+	* 
+	* Config
+	* Class constructor
+	* @author  Guillermo
+	* @since   2020-04-30
+	*  
+	*/
 	public Config() {
 		Properties prop = new Properties();
 		InputStream is = null;
@@ -24,6 +38,15 @@ public class Config {
 		db = prop.getProperty("DB");
 	}
 	
+	/**
+	* 
+	* getDb
+	* Method for getting the db url from the config.properties file.
+	* @return db database absolute path.
+	* @author  Guillermo
+	* @since   2020-04-30
+	*  
+	*/
 	public String getDb () {
 		return db;
 	}
